f03658bd3b405f7b95f1fe08a147910d2e807c06
isaac <exolivil@gamil.com>
2022-02-08 19:50:35 -0600

    Initial Commit

---
+++ CrytoWallet/CrytoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoWalletApp.swift
+//  CrytoWallet
+//
+//  Created by Isaac on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CrytoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
